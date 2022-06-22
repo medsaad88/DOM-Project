0165e27e3be52428a5484828e7ab072bc2e9dac7
medsaad88 <mohamedsaad.othman@gmail.com>
2022-06-23 00:42:32 +0100

    dom

---
+++ productsjava.js
@@ -0,0 +1,56 @@
+const products = [
+    {
+      id: 0,
+      name: "Bag Relax",
+      price: 20.00,
+      instock: 0,
+      description:
+        "3afsa Jdida",
+      imgSrc: "./t1.png",
+    },
+    {
+      id: 1,
+      name: "Bag Warning",
+      price: 21.00,
+      instock: 20,
+      description:
+        "3afsa Jdida",
+      imgSrc: "./t2.png",
+    },
+    {
+      id: 2,
+      name: "Bag Why?",
+      price: 25.00,
+      instock: 10,
+      description:
+        "3afsa Jdida",
+      imgSrc: "./t3.png",
+    },
+    {
+      id: 3,
+      name: "Bag Angry",
+      price: 30.00,
+      instock: 5,
+      description:
+        "3afsa Jdida",
+        imgSrc: "./t4.png",
+    },
+    {
+      id: 4,
+      name: "Bag Sorry",
+      price: 19.00,
+      instock: 4,
+      description:
+        "3afsa Jdida",
+      imgSrc: "./t5.png",
+    },
+    {
+      id: 5,
+      name: "Bag Lost",
+      price: 27.00,
+      instock: 40,
+      description:
+        "3afsa Jdida",
+      imgSrc: "./t6.png",
+    },
+  ];
